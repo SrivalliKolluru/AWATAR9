85607da9843c000cacbd68ccdc15fe307590cf93
Geeta-Balakrishnan <madison@awatar9.com>
2026-03-03 12:09:34 +0530

    Trigger build for new secrets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
+// Trigger build - 2026-03-03
 const nextConfig = {
     output: 'export', // Enabled for GitHub Pages deployment
     basePath: isProd ? '/AWATAR9' : '',
